a32e7448fe986fa00dfc1fb2cff42220d0ef11f1
Telkom Design Team <53335869+telkom-design@users.noreply.github.com>
2022-07-20 01:47:24 +0700

    Update build.js

--- build.js
+++ build.js
@@ -27,7 +27,10 @@ StyleDictionaryPackage.registerFormat({
 StyleDictionaryPackage.registerTransform({
     name: 'sizes/px',
     type: 'value',
-    matcher: (prop) => typeof prop.value === "string" && prop.value.indexOf("px") !== -1,
+    matcher: function(prop) {
+        // You can be more specific here if you only want 'em' units for font sizes    
+        return typeof prop.value === "string" && prop.value.indexOf("px") !== -1;
+    },
     transformer: function(prop) {
         // You can also modify the value here if you want to convert pixels to ems
         return parseFloat(prop.original.value) + 'px';
