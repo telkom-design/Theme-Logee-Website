37224c873bdc7173623cb94a41e1a486612b5852
Telkom Design Team <53335869+telkom-design@users.noreply.github.com>
2022-07-24 05:55:15 +0700

    bug fix

--- src/index copy.ts
+++
@@ -1,8 +0,0 @@
-import deepmerge from 'deepmerge'
-
-export * from './button'
-export * from './colors'
-export * from './spacers'
-export * from './thypograpy'
-export * from './theme'
-export { deepmerge as merge }
--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,4 @@ import deepmerge from 'deepmerge'
 
 export * from './colors'
 export * from './spacers'
-export * from './thypograpy'
-export * from './theme'
 export { deepmerge as merge }
