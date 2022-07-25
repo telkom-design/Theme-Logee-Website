d7761f967d783a58dab8185009b7df3afa62d8ff
Telkom Design Team <53335869+telkom-design@users.noreply.github.com>
2022-07-25 20:18:01 +0700

    Add typography and foundation to index

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,8 @@ import deepmerge from 'deepmerge'
 
 export * from './colors'
 export * from './spacers'
+export * from './thypograpy'
+export * from './tokens/foundation'
 export * from './theme-lgt'
 export * from './theme-lgd'
 export { deepmerge as merge }
